036b1dea428a84de5b00cb3f3a43b25082276a38
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-07-19 19:34:49 +0300

    .

--- routes/ads.js
+++ routes/ads.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Ads = require("../models/ads");
 const router = express.Router();
+const multer = require("multer");
 const upload = require("../middlewares/uploads");
 const { sendNotificationToRole } = require('../services/notifications');
 
